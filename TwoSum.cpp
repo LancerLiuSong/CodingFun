e5f367a22a850f37ed4fb9be090b25ec6b74b5f8
Song Liu <sl796@uowmail.edu.au>
2018-08-14 18:03:06 +1000

    setting up repo

---
+++ TwoSum.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/two-sum/description/
+include <algorithm>
+
+class Solution 
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        std::sort(nums.begine(). nums.end());
+        
+    }
+};
